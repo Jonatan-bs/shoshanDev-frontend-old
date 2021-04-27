6891cfb20f027c1ca108671514294adfa2f154de
Jonatan-bs <jonatan-bs@live.dk>
2021-04-27 19:48:20 +0200

    minor cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -23,7 +23,7 @@ function MyApp({ Component, pageProps, router }) {
   }, []);
 
     return (
-      <>
+      <>           
         <Head>
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
@@ -31,6 +31,12 @@ function MyApp({ Component, pageProps, router }) {
           <link rel="manifest" href="/site.webmanifest" />
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
           <meta name="theme-color" content="#ffffff" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+          <link rel="preconnect" href="https://fonts.gstatic.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
+          <meta charSet="UTF-8"/>
+          {/* <script type="text/javascript" src="/static/app.js"></script> */}
+          <title>Shoshan Development</title>
         </Head>
         <ThemeProvider theme={theme}>
           <GlobalStyle />
