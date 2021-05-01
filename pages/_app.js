57e5a59d0998fa28f96903c22adcf5f0cac16231
Jonatan-bs <jonatan-bs@live.dk>
2021-05-01 09:09:20 +0200

    Moved google fontface to globalStyle

--- pages/_app.js
+++ pages/_app.js
@@ -82,7 +82,6 @@ function MyApp({ Component, pageProps, router, error }) {
           <meta name="theme-color" content="#ffffff"/>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
-          <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400&display=swap" rel="stylesheet"></link>
           <meta charSet="UTF-8"/>
           <script type="text/javascript" src="/static/app.js"></script>
         </Head>
